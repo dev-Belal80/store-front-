a9a651bf94116c76cb979959aec3e9ea382346f1
mohammed <mhmdbdaljwad759@gmail.com>
2026-03-04 17:55:27 +0200

    add production env

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(
